4a8a3876c5f2e2616611ae8ddd0fcff24f22c96e
HHH <3403735819@qq.com>
2025-12-16 11:28:04 +0800

    实现us级延时函数

--- USER/main.c
+++ USER/main.c
@@ -1,6 +1,6 @@
 #include "gd32f10x.h"
 #include "usart.h"
-
+#include "delay.h"
 uint16_t i;
 
 
@@ -9,24 +9,29 @@ int main(void)
 	Usart0_Init(921600);
 	u0_printf("%d %c %x\r\n",0x30,0x30,0x30);
 	
+	Delay_Init();
+	
 	while(1)
 	{
-		if(U0CB.URxDataOUT != U0CB.URxDataIN)
-		{
-			u0_printf("this time get %d data\r\n",U0CB.URxDataOUT->end - U0CB.URxDataOUT->start +1);
-			for(i = 0; i < U0CB.URxDataOUT->end - U0CB.URxDataOUT->start +1; i++)
-			{
-				u0_printf("%c",U0CB.URxDataOUT->start[i]);
-			}
-			u0_printf("\r\n\n\r");
-			
-			U0CB.URxDataOUT++;
-		  if(U0CB.URxDataOUT == U0CB.URxDataEND)
-		  {
-			  U0CB.URxDataOUT = &U0CB.URxDataPtr[0];
-	  	}
-			
-		}
+		// ´®¿Ú²âÊÔ´úÂë
+//		if(U0CB.URxDataOUT != U0CB.URxDataIN)
+//		{
+//			u0_printf("this time get %d data\r\n",U0CB.URxDataOUT->end - U0CB.URxDataOUT->start +1);
+//			for(i = 0; i < U0CB.URxDataOUT->end - U0CB.URxDataOUT->start +1; i++)
+//			{
+//				u0_printf("%c",U0CB.URxDataOUT->start[i]);
+//			}
+//			u0_printf("\r\n\n\r");
+//			
+//			U0CB.URxDataOUT++;
+//		  if(U0CB.URxDataOUT == U0CB.URxDataEND)
+//		  {
+//			  U0CB.URxDataOUT = &U0CB.URxDataPtr[0];
+//	  	}
+//			
+//		}
+		  Delay_Ms(2000);
+			u0_printf("%d Delay succeed\r\n",0x30);
 		
 	}
 	
